2a48e206d766bfe9ed7d99305539fb0417ebc496
mfire <mfire@MACIEK>
2022-10-12 15:30:04 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract class ProductPrototype
+{
+    public decimal Price { get; set; }
+
+    public ProductPrototype(decimal price)
+    {
+        Price = price;
+    }
+
+    public ProductPrototype Clone() =>
+        (ProductPrototype)this.MemberwiseClone();
+}
+
+public sealed class Bread : ProductPrototype
+{
+    public Bread(decimal price) : base(price) { }
+}
+
+public sealed class Butter : ProductPrototype
+{
+    public Butter(decimal price) : base(price) { }
+}
+
+public sealed class Supermarket
+{
+    private Dictionary<string, ProductPrototype> _productList = new Dictionary<string, ProductPrototype>();
+
+    public void AddProduct(string key, ProductPrototype productPrototype)
+    {
+        _productList.Add(key, productPrototype);
+    }
+
+    public ProductPrototype GetClonedProduct(string key) =>
+        _productList[key].Clone();
+}
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Supermarket supermarket = new Supermarket();
+
+        supermarket.AddProduct("Bread", new Bread(9.50m));
+        supermarket.AddProduct("Butter", new Butter(5.30m));
+
+        var product = supermarket.GetClonedProduct("Bread");
+        Console.WriteLine(String.Format("Bread - {0} zł", product.Price));
+
+        product = supermarket.GetClonedProduct("Butter");
+        Console.WriteLine(String.Format("Butter - {0} zł", product.Price));
+    }
+}
